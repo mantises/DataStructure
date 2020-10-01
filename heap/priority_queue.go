45f6c61502c99168df0d33b089eee7865b066605
mingwei <mwguo07@gmail.com>
2020-10-02 03:09:00 +0800

    implement priority queue with heap

---
+++ heap/priority_queue.go
@@ -0,0 +1,59 @@
+package heap
+
+type FuncCmp func(x interface{}, y interface{}) bool
+
+type Heap struct {
+	val []interface{}
+	cmp FuncCmp
+}
+
+func NewHeap(cmp FuncCmp) Heap {
+	return Heap{
+		val: make([]interface{}, 0, 0),
+		cmp: cmp,
+	}
+}
+
+func (h *Heap) Push(x interface{})  {
+	h.val = append(h.val, x)
+	for i := len(h.val)/2 - 1; i >= 0; i-- {
+		h.adjustHeap(h.val, i, len(h.val))
+	}
+}
+
+func (h *Heap) Pop() interface{} {
+	if len(h.val) == 0 {
+		return nil
+	}
+	ret := h.val[0]
+	h.val = h.val[1:]
+	for i := len(h.val)/2 - 1; i >= 0; i-- {
+		h.adjustHeap(h.val, i, len(h.val))
+	}
+	return ret
+}
+
+func (h *Heap) adjustHeap(nums []interface{}, pos, length int) {
+	for {
+		child := pos*2 + 1
+		if child > length-1 {
+			break
+		}
+		if child < length-1 && h.cmp(nums[child+1], nums[child]) {
+			child++
+		}
+		if h.cmp(nums[child], nums[pos]) {
+			nums[pos], nums[child] = nums[child], nums[pos]
+			pos = child
+		} else {
+			break
+		}
+	}
+}
+
+func (h *Heap) Sort(nums []int) {
+	for i := len(nums) - 1; i > 0; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+		h.adjustHeap(h.val, 0, i)
+	}
+}
---
+++ heap/priority_queue_test.go
@@ -0,0 +1,29 @@
+package heap
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewHeap(t *testing.T) {
+	f := func(x interface{}, y interface{}) bool {
+		if x.(int) > y.(int) {
+			return true
+		}
+		return false
+	}
+	h := NewHeap(f)
+	h.Push(10)
+	h.Push(99)
+	h.Push(6)
+	h.Push(27)
+	h.Push(88)
+	fmt.Println(h.val)
+	fmt.Println(h.Pop())
+	fmt.Println(h.val)
+	h.Push(1000)
+	h.Push(90)
+	fmt.Println(h.val)
+	fmt.Println(h.Pop())
+	fmt.Println(h.val)
+}
